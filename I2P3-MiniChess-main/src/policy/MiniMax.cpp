0d787478947c60b8f489d5d8bc175c09cc8dee0c
JinWeijerry <jerry48599597@gapp.nthu.edu.tw>
2023-06-15 23:31:15 +0800

    Minimax 3

--- I2P3-MiniChess-main/src/policy/MiniMax.cpp
+++ I2P3-MiniChess-main/src/policy/MiniMax.cpp
@@ -11,23 +11,7 @@
  * @param depth You may need this for other policy
  * @return Move 
  */
-/*
-Move MiniMax::get_move(State *state, int depth){
-  if(!state->legal_actions.size())
-    state->get_legal_actions();
-  
-  auto actions = state->legal_actions;
-  int id=0;
-  int maxval=0;
-  for(long unsigned int i=0;i<actions.size();i++){
-    int val=(state->next_state(actions[i]))->evaluate();
-    if(val>=maxval){
-        id=i;
-        maxval=val;
-    }
-  }
-  return actions[id];
-}*/
+
 Move MiniMax::get_move(State *state, int depth){
   if(!state->legal_actions.size())
     state->get_legal_actions();
